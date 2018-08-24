b5d01c9fcb1cd30633b59684cbec9eb9ec00c382
BenjaminDoran <benadoran@gmail.com>
2018-08-24 09:15:12 -0400

    initial commit

---
+++ install.r
@@ -0,0 +1,2 @@
+install.packages(c("ROCR", "ggplot2", "Hmisc", "reshape", "gplots","stringr", "NMF",  "mixtools", "lars", "reshape2", "vioplot","fastICA", "tsne", "Rtsne", "fpc", "ape", "VGAM", "gdata",     "knitr", "useful", "hdf5r"))
+install.packages("Seurat")
